f3dde911aa10a02dcde7dd4cfbbd260bbcbf7c88
Beñat Zaldua <benatzaldua1775@gmail.com>
2015-10-02 11:43:44 +0200

    Labo3

--- IkusiErabiltzaileak.php
+++ IkusiErabiltzaileak.php
@@ -1,6 +1,6 @@
 <?php 
-mysql_connect("localhost","root","") or die(mysql_error());
-mysql_select_db("quiz") or die(mysql_error());
+mysql_connect("mysql.hostinger.es","u526113874_rb15","") or die(mysql_error());
+mysql_select_db("u526113874_quiz") or die(mysql_error());
 
 $erabiltzaileak = mysql_query("select * from Erabiltzaile");
 echo "<table border = 1><tr><th>Izena</th><th>Abizena</th><th>Emaila</th><th>Espezialitatea</th><th>Interesa Teknologian</th></tr>";
