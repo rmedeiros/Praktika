63acaa04edfe9395d6d8a8e41dbb0739163eceb6
Beñat Zaldua <benatzaldua1775@gmail.com>
2015-10-02 11:51:10 +0200

    Erroreen zuzenketa

--- IkusiErabiltzaileak.php
+++ IkusiErabiltzaileak.php
@@ -2,7 +2,7 @@
 mysql_connect("mysql.hostinger.es","u526113874_rb15","123456789") or die(mysql_error());
 mysql_select_db("u526113874_quiz") or die(mysql_error());
 
-$erabiltzaileak = mysql_query("select * from Erabiltzaile");
+$erabiltzaileak = mysql_query("select * from erabiltzaile");
 echo "<table border = 1><tr><th>Izena</th><th>Abizena</th><th>Emaila</th><th>Espezialitatea</th><th>Interesa Teknologian</th></tr>";
 
 while($row = mysql_fetch_array($erabiltzaileak)){
